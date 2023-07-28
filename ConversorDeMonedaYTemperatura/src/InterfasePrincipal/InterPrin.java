f07a0600fdee837155974bb9b8ed8381109482c4
Mershar0 <129344681+Mershar0@users.noreply.github.com>
2023-07-28 03:18:08 -0400

    Función de validación agregada

--- ConversorDeMonedaYTemperatura/src/InterfasePrincipal/InterPrin.java
+++ ConversorDeMonedaYTemperatura/src/InterfasePrincipal/InterPrin.java
@@ -21,37 +21,66 @@ public class InterPrin {
 			switch(options) {
 			case "Conversor de Monedas":
 				String input0 = JOptionPane.showInputDialog(null, "Ingrese la cantidad de dinero a convertir");
-				double receiveMValue = Double.parseDouble(input0);
-				convertM.ConvertMon(receiveMValue);
 				
-				int mAnswer = JOptionPane.showConfirmDialog(null, "¿Desea realizar otra conversión?");
-				if(JOptionPane.OK_OPTION == mAnswer) {
-					System.out.println("Entra");
-				} else {
-					JOptionPane.showMessageDialog(null, "Programa finalizado");
-				}
-				break;
+				if(validNum(input0) == true) {
+                    double receiveMValue = Double.parseDouble(input0);
+					convertM.ConvertMon(receiveMValue);
+					
+					int mAnswer = JOptionPane.showConfirmDialog(null, "¿Desea realizar otra conversión?");
+					if(JOptionPane.OK_OPTION == mAnswer) {
+						System.out.println("Afirmación");
+					} else {
+						JOptionPane.showMessageDialog(null, "Programa finalizado");
+					}	
+					} else {
+		                 JOptionPane.showMessageDialog(null, "Valor inválido");                
+		             }
+					break;
 			
 			
 			case "Conversor de Temperatura":
 				String input1 = JOptionPane.showInputDialog(null, "Ingrese la temperatura a convertir");
-				double receiveTValue = Double.parseDouble(input1);
-				convertT.ConvertTemp(receiveTValue);
 				
-				int tAnswer = JOptionPane.showConfirmDialog(null, "¿Desea realizar otra conversión?");
-				if(JOptionPane.OK_OPTION == tAnswer) {
-					System.out.println("Entra");
-				} else {
-					JOptionPane.showMessageDialog(null, "Programa finalizado");
-				}
-				break;
+				 if(validNum(input1) == true) {
+					 double receiveTValue = Double.parseDouble(input1);
+					 convertT.ConvertTemp(receiveTValue); 
+					
+					 int tAnswer = JOptionPane.showConfirmDialog(null, "¿Desea realizar otra conversión?");
+					 if((tAnswer == 0) && (validNum(input1) == true)){
+						 
+				 } else{
+						JOptionPane.showMessageDialog(null, "Programa finalizado");
+					} 
+					 
+				 } else {
+		                 JOptionPane.showMessageDialog(null, "Valor inválido");                
+		             }
+					break;
+					 
+					 
+			}
 		}
+		}
+					
+					
 			
-		}
 		
-		
-	}
+	
+	
+	 public static boolean validNum(String input) {
+	        try {
+	            double x = Double.parseDouble(input);
+	            if(x >= 0 || x < 0);
+	                return true;
+	            } catch (NumberFormatException e) {
+	                return false;
+	            }
+	    }
+	
 }
+		
+	
+
 	
 	
 	
